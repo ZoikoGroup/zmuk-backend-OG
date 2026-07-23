fd68e4d13dc3afabc70cd0129f74489c64b7e96f
DeyDebasish2003 <04debasish03@gmail.com>
2026-07-23 17:16:25 +0530

    requirement changed

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
 
+load_dotenv()
 # BASE_DIR points to the root of your Django project
 BASE_DIR = Path(__file__).resolve().parent.parent
 
